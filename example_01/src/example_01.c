2545204e9bf080b6bd076ec9ba8099609f746ea2
Михаил Ряполов <rmish@yandex.ru>
2019-03-22 09:39:02 +0300

    Create project

---
+++ example_01/src/example_01.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : example_01.c
+ Author      : Котик
+ Version     :
+ Copyright   : nothing
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Мяф!"); /* prints Мяф! */
+	return EXIT_SUCCESS;
+}
